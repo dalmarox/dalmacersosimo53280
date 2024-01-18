e4fad159b35e53eb232322c8fd514772f5946c8b
Dalma Cersosimo <cersosimodalma@gmail.com>
2024-01-18 18:05:53 -0300

    clase 1

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}> Hola mundo, soy Dalma!
+      Estoy creando mi primer App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+    fontStyle:'italic',
   },
+  text: {
+    fontSize:35,
+    textAlign:'center',
+    color:'white',
+  }
 });
